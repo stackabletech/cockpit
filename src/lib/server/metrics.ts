61525776b8879b9d82ad0036eb436361bdc110b8
Benedikt Labrenz <benedikt@labrenz.org>
2026-02-23 17:23:20 +0100

    reduce http request metric buckets

--- src/lib/server/metrics.ts
+++ src/lib/server/metrics.ts
@@ -8,6 +8,6 @@ export const httpRequestDuration = new Histogram({
   name: 'http_request_duration_seconds',
   help: 'Duration of HTTP requests in seconds',
   labelNames: ['method', 'route', 'status'],
-  buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10],
+  buckets: [0.25, 1.0],
   registers: [register]
 });
