b9faf5bd5f4dedf45b46461a51427adc58d8997c
Benedikt Labrenz <benedikt@labrenz.org>
2026-04-09 16:57:03 +0200

    add csv export and move trino deep link to new action bar within StatementResult

--- src/routes/(app)/trino/+page.svelte
+++ src/routes/(app)/trino/+page.svelte
@@ -806,14 +806,28 @@
               })}
             </span>
           {/if}
-          {#if runner.results.length === 1 && runner.results[0].trinoQueryUrl}
+          {#if runner.currentTrinoQueryUrl}
             <a
-              href={runner.results[0].trinoQueryUrl}
+              href={runner.currentTrinoQueryUrl}
               target="_blank"
               rel="noopener noreferrer"
-              class="link link-primary text-xs"
+              class="btn btn-xs btn-ghost"
             >
               {m.trino_view_in_trino()}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-3.5 w-3.5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"
+                />
+                <path
+                  d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"
+                />
+              </svg>
             </a>
           {/if}
         </div>
