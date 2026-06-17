c6fdd6ebf5e689151ea2edf39489b61bb75af59f
dklOrdix <dkl@ordix.de>
2026-06-17 15:32:41 +0200

    Fix import

--- e2e/auth/auth.spec.ts
+++ e2e/auth/auth.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { waitForHydration } from '../support/helpers.js';
 
 test.describe('Authentication', () => {
   test.use({ locale: 'en-US' });
