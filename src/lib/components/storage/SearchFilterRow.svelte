38c27d08308b4ad88bb6ef3062059f30c13a91f4
F3l1x1vo <fli@ordix.de>
2026-08-18 14:56:30 +0200

    fix lint

--- src/lib/components/storage/SearchFilterRow.svelte
+++ src/lib/components/storage/SearchFilterRow.svelte
@@ -60,7 +60,7 @@
     <label class="sr-only" for="{uid}-operator">{m.storage_search_filter_operator()}</label>
     <select
       id="{uid}-operator"
-      class="select select-sm border-base-300/0 h-auto min-h-0 w-14 flex-shrink-0 border-0 pe-7 ps-2 text-center font-mono text-sm"
+      class="select select-sm border-base-300/0 h-auto min-h-0 w-14 flex-shrink-0 border-0 ps-2 pe-7 text-center font-mono text-sm"
       value={filter.operator}
       onchange={(event) => setOperator(event.currentTarget.value as SearchFilterOperator)}
     >
